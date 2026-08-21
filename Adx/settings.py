97ca6ec135c7ac589021701f65235a75dfe3cef5
Andrew Halilu <114777821+Information14@users.noreply.github.com>
2026-08-21 14:36:35 +0100

    ADX Project

--- Adx/settings.py
+++ Adx/settings.py
@@ -91,8 +91,9 @@ WSGI_APPLICATION = 'Adx.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
+        os.environ.get("DATABASE_URL", ""),
         conn_max_age=600,
+        ssl_require=True,
     )
 }
 
